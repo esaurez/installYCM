e6645abcbef89ae05613273a574214eb1e7c8b97
esaurez <enrique.saurez@gmail.com>
2016-11-07 17:47:53 -0500

    Update install.sh

--- install.sh
+++ install.sh
@@ -4,7 +4,7 @@ sudo apt-get install -y libncurses5-dev libgnome2-dev libgnomeui-dev \
     libgtk2.0-dev libatk1.0-dev libbonoboui2-dev \
     libcairo2-dev libx11-dev libxpm-dev libxt-dev python-dev \
     python3-dev ruby-dev lua5.1 lua5.1-dev libperl-dev git checkinstall\
-    build-essential python-dev python3-dev
+    build-essential python-dev python3-dev clang
 sudo apt-get remove -y vim vim-runtime gvim vim-common
 mkdir -p ~/src
 cd ~/src
